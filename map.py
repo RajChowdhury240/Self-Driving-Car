3a28ba741217b7f4c0c0c377d6c6bd8b0b51365f
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-12 10:54:41 +0530

    Update map.py

--- map.py
+++ map.py
@@ -18,24 +18,29 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 
 # Importing the Dqn object from our AI in ai.py
+
 from ai import Dqn
 
 # Adding this line if we don't want the right click to put a red point
+
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 # Introducing last_x and last_y, used to keep the last point in memory when we draw the sand on the map
+
 last_x = 0
 last_y = 0
 n_points = 0
 length = 0
 
 # Getting our AI, which we call "brain", and that contains our neural network that represents our Q-function
+
 brain = Dqn(5,3,0.9)
 action2rotation = [0,20,-20]
 last_reward = 0
 scores = []
 
 # Initializing the map
+
 first_update = True
 def init():
     global sand
@@ -48,6 +53,7 @@ def init():
     first_update = False
 
 # Initializing the last distance
+
 last_distance = 0
 
 # Creating the car class
@@ -232,6 +238,7 @@ class CarApp(App):
         brain.load()
 
 # Running the whole thing
+
 if __name__ == '__main__':
     CarApp().run()
     
